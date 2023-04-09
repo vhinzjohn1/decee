09f4edf26cf960b372567a8ed42bab8e2eb3a2f0
vhinzjohn1 <77475601+vhinzjohn1@users.noreply.github.com>
2023-04-09 18:10:52 +0800

    changes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let counter = 0;
     const prompts = [
     "Hi Ashh🤍",
-    "Just wanted to brighten up your day with a little greetings from me🤍",
-    "Goodmorning🤍 Have a Great Day ahead ashhh🤍",
+    "Just wanted to brighten up your day with some greetings from me🤍",
+    "You always brighten up my day, and I hope your day is as awesome as you are🤍",
+    "Ready to start the day with a smile? Goodmorniggg baby girl🤍",
     ];
     
     function showText() {
@@ -26,27 +27,33 @@ let counter = 0;
 
     
     function nextPrompt() {
-    if (counter >= 2) {
+      if (counter >= 3) {
         const promptContainer = document.getElementById("promptContainer");
         promptContainer.style.opacity = 0;
         setTimeout(() => {
-        counter = 0;
-        const promptButton = document.getElementById("promptButton");
-        promptButton.style.display = "block";
-        promptContainer.style.display = "none";
-        promptContainer.style.opacity = 1;
+          promptContainer.style.display = "none";
+          const promptButton = document.getElementById("promptButton");
+          promptButton.style.display = "block";
         }, 750);
-    } else {
+        const nextButton = document.getElementById("nextButton");
+        nextButton.textContent = "Next";
+        counter = 0;
+      } else {
         counter++;
         const promptContainer = document.getElementById("promptContainer");
         promptContainer.style.opacity = 0;
         setTimeout(() => {
-        const promptText = document.getElementById("promptText");
-        promptText.innerHTML = `${prompts[counter]}`;
-        promptContainer.style.opacity = 1;
+          const promptText = document.getElementById("promptText");
+          promptText.innerHTML = `${prompts[counter]}`;
+          promptContainer.style.opacity = 1;
+          const nextButton = document.getElementById("nextButton");
+          nextButton.textContent = (counter >= 3) ? "Done" : "Next";
         }, 750);
+      }
     }
-    }
+    
+    
+      
 
     const button = document.getElementById("promptButton");
     button.addEventListener("click", showText);
